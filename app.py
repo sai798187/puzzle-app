f6d99e55949886f679b7873a0a5328b487d13694
ROGAN07sg <shekhargupta349@gmail.com>
2026-04-03 13:28:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Puzzle App Running Successfully 🚀"
+    return render_template("index.html")
 
 @app.route("/health")
 def health():
